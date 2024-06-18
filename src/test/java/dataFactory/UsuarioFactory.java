f219f232c1115c359523d521dda470820287845a
Weslley Carvalho <147743364+weslley-carvalho11@users.noreply.github.com>
2024-06-18 18:45:12 -0300

    Update UsuarioFactory.java
    Adicionando nome e senha de usuários genericos

--- src/test/java/dataFactory/UsuarioFactory.java
+++ src/test/java/dataFactory/UsuarioFactory.java
@@ -5,8 +5,8 @@ import pojo.UsuarioPojo;
 public class UsuarioFactory {
     public static UsuarioPojo criarUsuario (){
         UsuarioPojo usuario = new UsuarioPojo();
-        usuario.setUsuarioLogin("admin");
-        usuario.setUsuarioSenha("admin");
+        usuario.setUsuarioLogin("nome_do_usuario");
+        usuario.setUsuarioSenha("senha_do_usuario");
         return usuario;
     }
 }
